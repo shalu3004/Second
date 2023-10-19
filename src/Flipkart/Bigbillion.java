34c51b42de83ddce610c933cf1775198b07787b4
Shalinu Saju <shalinu.saju@codinghands.in>
2023-10-19 12:08:42 +0530

    url added

--- src/Flipkart/Bigbillion.java
+++ src/Flipkart/Bigbillion.java
@@ -1,9 +1,15 @@
 package Flipkart;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Bigbillion {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\codin\\eclipse-workspace\\Second\\chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.flipkart.com");
+        driver.manage().window().maximize();
 
 	}
 
